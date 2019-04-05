f5334472eda7f043a9e6a085548fb31e11c322b4
sirin <sirin_md@hotmail.com>
2019-04-05 11:00:04 +0700

    cleanup

--- toTopArrow.js
+++
@@ -1,9 +0,0 @@
-function scroll() {
-    topArr = document.getElementsByClassName("toTop");
-    
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
-        topArr[0].style.display="inline"}
-       
-    else {
-        topArr[0].style.display="none"}
-}
